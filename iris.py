f99c6c703d80a97a5fad1332e66d1c25e26b0912
adityakrshnn <adityakrshnn@gmail.com>
2017-12-01 23:52:43 +0530

    Add files via upload

---
+++ iris.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 14 01:37:05 2017
+
+@author: 76b2
+"""
+
+import numpy as np 
+import pandas as pd 
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression  # for Logistic Regression algorithm
+from sklearn.cross_validation import train_test_split #to split the dataset for training and testing
+from sklearn.neighbors import KNeighborsClassifier  # for K nearest neighbours
+from sklearn import svm  #for Support Vector Machine (SVM) Algorithm
+from sklearn import metrics #for checking the model accuracy
+from sklearn.tree import DecisionTreeClassifier #for using Decision Tree Algoithm
+
+iris = pd.read_csv('./Iris.csv')
+
+iris.head()
+
+fig = iris[iris.Species=='Iris-setosa'].plot(kind='scatter',x='SepalLengthCm',y='SepalWidthCm',color='orange', label='Setosa')
+iris[iris.Species=='Iris-versicolor'].plot(kind='scatter',x='SepalLengthCm',y='SepalWidthCm',color='blue', label='versicolor',ax=fig)
+iris[iris.Species=='Iris-virginica'].plot(kind='scatter',x='SepalLengthCm',y='SepalWidthCm',color='green', label='virginica', ax=fig)
+fig.set_xlabel("Sepal Length")
+fig.set_ylabel("Sepal Width")
+fig.set_title("Sepal Length VS Width")
+fig=plt.gcf()
+fig.set_size_inches(10,6)
+plt.show()
+
+fig = iris[iris.Species=='Iris-setosa'].plot.scatter(x='PetalLengthCm',y='PetalWidthCm',color='orange', label='Setosa')
+iris[iris.Species=='Iris-versicolor'].plot.scatter(x='PetalLengthCm',y='PetalWidthCm',color='blue', label='versicolor',ax=fig)
+iris[iris.Species=='Iris-virginica'].plot.scatter(x='PetalLengthCm',y='PetalWidthCm',color='green', label='virginica', ax=fig)
+fig.set_xlabel("Petal Length")
+fig.set_ylabel("Petal Width")
+fig.set_title(" Petal Length VS Width")
+fig=plt.gcf()
+fig.set_size_inches(10,6)
+plt.show()
+
+plt.figure(figsize=(14,10))
+plt.subplot(2,2,1)
+sns.violinplot(x='Species',y='PetalLengthCm',data=iris)
+plt.subplot(2,2,2)
+sns.violinplot(x='Species',y='PetalWidthCm',data=iris)
+plt.subplot(2,2,3)
+sns.violinplot(x='Species',y='SepalLengthCm',data=iris)
+plt.subplot(2,2,4)
+sns.violinplot(x='Species',y='SepalWidthCm',data=iris)
+
+iris.shape
+
+train, test = train_test_split(iris, test_size = 0.3)
+
+train_X = train[['SepalLengthCm','SepalWidthCm','PetalLengthCm','PetalWidthCm']]# taking the training data features
+train_y=train.Species# output of our training data
+test_X= test[['SepalLengthCm','SepalWidthCm','PetalLengthCm','PetalWidthCm']] # taking test data features
+test_y =test.Species   #output value of test data
+
+model = LogisticRegression()
+model.fit(train_X,train_y)
+prediction=model.predict(test_X)
+print('The accuracy of the Logistic Regression is',metrics.accuracy_score(prediction,test_y))
+
+model=DecisionTreeClassifier()
+model.fit(train_X,train_y)
+prediction=model.predict(test_X)
+print('The accuracy of the Decision Tree is',metrics.accuracy_score(prediction,test_y))
+
+model=KNeighborsClassifier(n_neighbors=6)
+model.fit(train_X,train_y)
+prediction=model.predict(test_X)
+print('The accuracy of the KNN is',metrics.accuracy_score(prediction,test_y))
+
+a_index=list(range(1,11))
+a=pd.Series()
+x=[1,2,3,4,5,6,7,8,9,10]
+for i in list(range(1,11)):
+    model=KNeighborsClassifier(n_neighbors=i) 
+    model.fit(train_X,train_y)
+    prediction=model.predict(test_X)
+    a=a.append(pd.Series(metrics.accuracy_score(prediction,test_y)))
+plt.plot(a_index, a)
+plt.xticks(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
